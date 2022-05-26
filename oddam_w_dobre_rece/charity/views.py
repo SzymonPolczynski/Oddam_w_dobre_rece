ac697637ce4ec0cffeed37af46d1d5d1b7714466
Szymon Połczyński <polczing@gmail.com>
2022-05-26 20:46:24 +0200

    html + static files modified, navigation + redirects added

--- oddam_w_dobre_rece/charity/views.py
+++ oddam_w_dobre_rece/charity/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from charity.models import Institution, Donation
+
+
+class LandingPageView(View):
+    def get(self, request):
+        institutions = Institution.objects.all().count()
+        donations = Donation.objects.all()
+        # quantity = []
+        # for donation in donations:
+        #     quantity.append(donation.quantity)
+        sum_of_bags = sum([donation.quantity for donation in donations])
+        ctx = {
+            "institutions": institutions,
+            "sum_of_bags": sum_of_bags,
+        }
+        return render(request, "index.html", context=ctx)
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, "form.html")
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, "register.html")
